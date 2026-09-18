1b5eda890754a1562b28f6816e832a84234e9867
rain joaquin <rainjoaquin2009@gmail.com>
2026-09-18 14:34:54 +0800

    Added ColorChanger, FilterUser, Settings, UserfFunction

--- app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/FilterUser.java
+++ app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/FilterUser.java
@@ -1,4 +1,27 @@
 package com.example.surelypresent.quarter2.MiniPeta_3;
 
+import java.util.Scanner;
+
 public class FilterUser {
+
+
+    public int execute(Scanner scanner, int user) {
+
+        System.out.print("Enter your designated user: ");
+        int Function = scanner.nextInt();
+
+        System.out.println(Function);
+
+        if (Function == 1) {
+            System.out.println("Welcome back dear student!");
+        } else if (Function == 2) {
+            System.out.println("Welcome, teacher!");
+        } else if (Function == 3) {
+            System.out.println("You are an admin!");
+        } else {
+            System.out.println("You are not from this school.");
+        }
+
+        return Function;
+    }
 }
