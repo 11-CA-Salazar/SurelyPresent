2e6f685b2db9a6b2d9f571952812414c85237c9c
clyde <2016-20429@mcstayuman.edu.ph>
2026-09-22 02:54:53 +0800

    Finished the settings, and updated the main menu

--- app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/MainMenu.java
+++ app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/MainMenu.java
@@ -12,16 +12,33 @@ public class MainMenu{
         System.out.println("#############");
         System.out.println("#############");
         System.out.println("#############");
-        System.out.println("#############");
+        System.out.println("#############\n");
         int choice = 1;
         boolean chosed = true;
         while (chosed) {
             if (choice == 1) {
-                System.out.println("=========");
+                System.out.println("================");
+                System.out.println("    Settings!   ");
+                System.out.println("================\n");
+                Settings settings = new Settings();
+                Settings.execute(scanner);
+
+            }else if (choice == 2){
+                System.out.println("================");
                 System.out.println("FILTERING USER!");
+                System.out.println("================\n");
                 FilterUser filterUser = new FilterUser();
                 FilterUser.execute(scanner);
-            } chosed = false;
+            }else if (choice == 3){
+                System.out.println("==============");
+                System.out.println("COLOR CHANGER!");
+                System.out.println("==============\n");
+                ColorChanger colorChanger = new ColorChanger();
+                ColorChanger.execute(scanner);
+            }else {
+                chosed = false;
+            }
+            choice++;
         }
     }
 }
--- app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/Settings.java
+++ app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/Settings.java
@@ -1,4 +1,81 @@
 package com.example.surelypresent.quarter2.MiniPeta_3;
 
+import java.util.Scanner;
 public class Settings {
+    public static void execute(Scanner scanner){
+
+        System.out.println("====1.PROFILE");
+        System.out.println("====2.NOTIFICATION");
+        System.out.println("====3.DARK MODE");
+        System.out.println("====4.PRIVACY");
+        System.out.println("====5.ABOUT");
+        System.out.println("====6.LOGOUT\n");
+
+        int choice = 1;
+        boolean chosed = true;
+
+        while(chosed){
+            switch(choice){
+                case 1:
+                    System.out.println("============");
+                    System.out.println("USER PROFILE");
+                    System.out.println("============\n");
+                    System.out.println("1.Change username");
+                    System.out.println("2.Change Password");
+                    System.out.println("3.Add email\n");
+                    choice++;
+                    break;
+                case 2:
+                    System.out.println("============");
+                    System.out.println("NOTIFICATION");
+                    System.out.println("============\n");
+                    System.out.println("1.On Notification");
+                    System.out.println("2.Off Notification\n");
+                    choice++;
+                    break;
+                case 3:
+                    System.out.println("============");
+                    System.out.println("  DARK MODE  ");
+                    System.out.println("============\n");
+                    System.out.println("1.On Darkmode");
+                    System.out.println("2.Off Darkmode\n");
+                    choice++;
+                    break;
+                case 4:
+                    System.out.println("============");
+                    System.out.println("   PRIVACY  ");
+                    System.out.println("============\n");
+                    System.out.println("1. Privacy Center");
+                    System.out.println("2. Privacy Checkup");
+                    System.out.println("3. Activity log\n");
+                    choice++;
+                    break;
+                case 5:
+                    System.out.println("============");
+                    System.out.println("    ABOUT    ");
+                    System.out.println("============\n");
+                    System.out.println("#############");
+                    System.out.println("#############");
+                    System.out.println("#############");
+                    System.out.println("#############");
+                    System.out.println("#############");
+                    System.out.println("#############\n");
+                    choice++;
+                    break;
+                case 6:
+                    System.out.println("=============");
+                    System.out.println("   LOG OUT   ");
+                    System.out.println("=============\n");
+                    chosed = false; // ENDS THE LOOP
+                    break;
+
+                default:
+                    chosed = false;
+                    break;
+            }
+        }
+
+
+
+    }
 }
