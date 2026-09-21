83285d4ee8a21d1e7519952f06e16408d27e0948
clyde <2016-20429@mcstayuman.edu.ph>
2026-09-22 00:36:29 +0800

    Revert "finished the filter used part and connected it with the Mainmenu"
    This reverts commit 9766e3cbbed03f08d5500c7fb92d4db2dbed71d3.

--- app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/FilterUser.java
+++ app/src/test/java/com/example/surelypresent/quarter2/MiniPeta_3/FilterUser.java
@@ -64,17 +64,6 @@ public class FilterUser {
                     break;
 
                 case 6:
-                    System.out.println("===WELCOME BACK ASST./SECRETARY===");
-                    System.out.println("##################################");
-                    System.out.println("##################################");
-                    System.out.println("##################################");
-                    System.out.println("##################################");
-                    System.out.println("##################################");
-                    System.out.println("##################################\n");
-                    choice++; //WILL SWITCH TO AUDITOR/TREASURER
-                    break;
-
-                case 7:
                     System.out.println("===WELCOME BACK TREASURER/AUDITOR===");
                     System.out.println("##################################");
                     System.out.println("##################################");
@@ -82,11 +71,11 @@ public class FilterUser {
                     System.out.println("##################################");
                     System.out.println("##################################");
                     System.out.println("##################################\n");
-                    choice++; //WILL SWITCH TO REGULAR STUDENT
+                    choice++; // WILL SWITCH TO REGULAR STUDENT
                     break;
 
-                case 8:
-                    System.out.println("====== WELCOME BACK DEAR STUDENT======");
+                case 7:
+                    System.out.println("=======WELCOME BACK DEAR STUDENT======");
                     System.out.println("##################################");
                     System.out.println("##################################");
                     System.out.println("##################################");
