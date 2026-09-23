cc1a162dc4aac661fb30c39fdf8281fbb9e9e1a9
ceddyb0y <acerbito12@gmail.com>
2026-09-23 14:54:47 +0800

    2nd commit, while loop structure

--- app/src/test/java/com/example/surelypresent/quarter2/practicalexam/FastFoodMenu.java
+++ app/src/test/java/com/example/surelypresent/quarter2/practicalexam/FastFoodMenu.java
@@ -2,7 +2,16 @@ package com.example.surelypresent.quarter2.practicalexam;
 
 import java.util.Scanner;
 public class FastFoodMenu {
-    public void start(Scanner scanner){
+    public void start(Scanner scanner) {
+        boolean ordering = true;
 
+        System.out.println("--- GENERATING FAST FOOD TEST DATA ---");
+        System.out.println("What would you like to order?");
+        System.out.println("1. Burger");
+        System.out.println("2. Fries");
+
+        while (ordering && scanner.hasNextLine()) {
+
+        }
     }
 }
