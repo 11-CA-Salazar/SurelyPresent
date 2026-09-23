11bce3a24e8c6923ee39264a62da194b2d8e6758
Clyde Salazar <2016-20429@mcstayuman.edu.ph>
2026-09-23 14:49:56 +0800

    2nd commit, did the structure of the while loop

--- app/src/test/java/com/example/surelypresent/quarter2/practicalexam/GymMenu.java
+++ app/src/test/java/com/example/surelypresent/quarter2/practicalexam/GymMenu.java
@@ -2,7 +2,20 @@ package com.example.surelypresent.quarter2.practicalexam;
 
 import java.util.Scanner;
 public class GymMenu {
-    public void start(Scanner scanner){
+    public void start(Scanner scanner) {
+        boolean lifting = true;
 
+
+        System.out.println("Does user want to enter the gym?");
+        System.out.println("1.yes");
+        System.out.println("2.No");
+        System.out.println("3.Exit\n");
+
+        while (lifting && scanner.hasNextLine()) {
+
+        }
     }
 }
+
+
+
