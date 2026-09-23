d06271686e6da119da5bd7031b375f721299c366
Rain Joaquin <rainjoaquin2009@gmail.com>
2026-09-23 20:19:31 +0800

    Added Ticket buying function on CinemaMenu

--- app/src/test/java/com/example/surelypresent/quarter2/practicalexam/CinemaMenu.java
+++ app/src/test/java/com/example/surelypresent/quarter2/practicalexam/CinemaMenu.java
@@ -8,21 +8,38 @@ public class CinemaMenu {
     // Step 3: Test snack purchase
     // Step 4: Exit system
     public static void start(Scanner scanner) {
+        System.out.println("======================================");
+        System.out.println("==== WOULD YOU LIKE TO BUY TICKET? ===");
+        System.out.println("======================================");
 
-        System.out.println("=========================");
-        System.out.println("==== WOULD YOU LIKE TO BUY TICKET?===");
-        System.out.println("=========================");
 
-        boolean buying = true;
+        boolean running = true;
 
-        while (buying && scanner.hasNext())
-            switch (buying) {
-                case 1:
-                    System.out.println("1. Yes");
+
+        while (running && scanner.hasNextLine()) {
+            String choice = scanner.nextLine().trim();
+            System.out.println("Does user want to buy a ticket?");
+            System.out.println("1. Yes");
+            System.out.println("2. No\n");
+            int age = scanner.nextInt();
+            switch (choice) {
+                case "1":
+                    if (choice.equals("1")) {
+                        System.out.println("User bought Tickets");
+                    } else if (choice.equals("2\n")) {
+                        System.out.println("User did not bought Tickets");
+                    }
+                    if (age < 18) {
+                        System.out.println("Access denied");
+                    } else if (age >= 18) {
+                        System.out.println("Ticket Printed");
+
+                    }
                     break;
-                case 2:
-                    System.out.println("2. No");
-                    break;
+                default:
             }
+            running = false;
+            }
+        }
     }
-}
+
