0a124cdff7794ff5bb38b01553cd4f7b2ec4076d
ceddyb0y <acerbito12@gmail.com>
2026-09-25 15:07:21 +0800

    commit #1

--- app/src/test/java/com/example/surelypresent/quarter2/practicalexam/FastFoodMenu.java
+++ app/src/test/java/com/example/surelypresent/quarter2/practicalexam/FastFoodMenu.java
@@ -1,6 +1,8 @@
+// File: FastFoodMenu.java
 package com.example.surelypresent.quarter2.practicalexam;
 
 import java.util.Scanner;
+
 public class FastFoodMenu {
     public void start(Scanner scanner) {
         boolean ordering = true;
@@ -10,27 +12,5 @@ public class FastFoodMenu {
         System.out.println("1. Burger");
         System.out.println("2. Fries");
         System.out.println("3. Exit");
-
-        while (ordering && scanner.hasNextLine()) {
-
-            String input = scanner.nextLine().trim();
-
-            switch(input){
-                case "1":
-                    System.out.println("Succesfully ordered a burger");
-                    break;
-                case "2":
-                    System.out.println("Would you like to upgrade to a combo meal?");
-                    System.out.println("1. Yes");
-                    System.out.println("2. No");
-                    if (scanner.hasNextLine()){
-                        String tierInput = scanner.nextLine().trim();
-                        if(tierInput.equals("1")){
-                            System.out.println("Succesfully upgraded to a combo meal");
-                        } else if(tierInput.equals("2")){
-                            System.out.println("succesfully ");
-                          break;
-                        }
-                    }
-
+    }
 }
